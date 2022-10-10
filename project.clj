a62fdd6242fc4473e81fef8218f87ec34c3a9733
Greg Haskins <greg@manetu.com>
2022-10-10 17:59:23 -0400

    Update deps
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,33 +1,33 @@
 (defproject manetu/data-loader "1.0.1-SNAPSHOT"
   :description "A utility to load and verify data on the Manetu Platform"
   :url "https://github.com/manetu/data-loader"
-  :plugins [[lein-cljfmt "0.8.0"]
+  :plugins [[lein-cljfmt "0.9.0"]
             [lein-kibit "0.1.8"]
             [lein-bikeshed "0.5.2"]
-            [lein-cloverage "1.2.2"]
-            [jonase/eastwood "1.2.3"]
+            [lein-cloverage "1.2.3"]
+            [jonase/eastwood "1.3.0"]
             [lein-bin "0.3.5"]]
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/tools.cli "1.0.206"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.cli "1.0.214"]
                  [io.pedestal/pedestal.service "0.5.10"]
-                 [io.github.protojure/grpc-client "2.0.9"]
-                 [io.github.protojure/google.protobuf "2.0.0"]
-                 [com.google.protobuf/protobuf-java "3.19.4"]
-                 [com.taoensso/timbre "5.1.2"]
+                 [io.github.protojure/grpc-client "2.4.1"]
+                 [io.github.protojure/google.protobuf "2.0.1"]
+                 [com.google.protobuf/protobuf-java "3.21.7"]
+                 [com.taoensso/timbre "5.2.1"]
                  [com.fzakaria/slf4j-timbre "0.3.21"]
-                 [org.slf4j/jul-to-slf4j "1.7.36"]
-                 [org.slf4j/jcl-over-slf4j "1.7.36"]
-                 [org.slf4j/log4j-over-slf4j "1.7.36"]
-                 [org.eclipse.jetty.http2/http2-client "11.0.8"]
-                 [org.eclipse.jetty/jetty-alpn-java-client "11.0.8"]
-                 [cheshire "5.10.2"]
+                 [org.slf4j/jul-to-slf4j "2.0.3"]
+                 [org.slf4j/jcl-over-slf4j "2.0.3"]
+                 [org.slf4j/log4j-over-slf4j "2.0.3"]
+                 [org.eclipse.jetty.http2/http2-client "11.0.12"]
+                 [org.eclipse.jetty/jetty-alpn-java-client "11.0.12"]
+                 [cheshire "5.11.0"]
                  [danlentz/clj-uuid "0.1.9"]
                  [org.clojure/core.match "1.0.0"]
                  [progrock "0.1.2"]
                  [doric "0.9.0"]
                  [clj-commons/fs "1.6.310"]
-                 [org.clojure/data.csv "1.0.0"]
-                 [kixi/stats "0.5.4"]
+                 [org.clojure/data.csv "1.0.1"]
+                 [kixi/stats "0.5.5"]
                  [de.ubercode.clostache/clostache "1.4.0"]
                  [buddy/buddy-sign "3.4.333"]]
   :main ^:skip-aot manetu.data-loader.main
@@ -48,5 +48,5 @@
   :repl-options {:init-ns user}
 
   :profiles {:dev {:dependencies [[clj-http "3.12.3"]
-                                  [org.clojure/tools.namespace "1.2.0"]]}
+                                  [org.clojure/tools.namespace "1.3.0"]]}
              :uberjar {:aot :all}})
