35b41e0cb08579596d45039d322003d25174f037
Greg Haskins <greg@manetu.com>
2022-11-08 14:29:30 -0500

    Release v1.1.0
    Changes since v1.0.0
    --------------------
    15cc06b Add support for new vault-service API
    a66a6b3 Update deps
    d49c1df Add installation instructions
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject manetu/data-loader "1.0.1-SNAPSHOT"
+(defproject manetu/data-loader "1.1.0"
   :description "A utility to load and verify data on the Manetu Platform"
   :url "https://github.com/manetu/data-loader"
   :plugins [[lein-cljfmt "0.9.0"]
